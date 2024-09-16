08abb05f1009113a207080d8219a3007be051971
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-09-16 09:35:41 +0300

    asdasd

--- src/lib/suzand/index.js
+++ src/lib/suzand/index.js
@@ -3,7 +3,7 @@ import { create } from "zustand";
  export const useAppStore = create((set) => ({
    admin:  JSON.parse(localStorage.getItem("admin")) || null,
    flowers: null,
-   addItemModal: true,
+   addItemModal: false,
    setAdmin: (admin) => set(() => {
          if(admin) {
           localStorage.setItem("admin",JSON.stringify(admin))
